f8fd1b00d6de767e01209c899129f1e12c1ef722
Duncan McClean <duncan@mcclean.co.uk>
2020-05-27 09:44:43 +0100

    composer update

---
+++ tests/DuplicateActionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DoubleThreeDigital\Duplicator\Tests;
+
+class DuplicateActionTest extends TestCase
+{
+    /** @test */
+    public function can_duplicate_entry()
+    {
+        //
+    }
+}
